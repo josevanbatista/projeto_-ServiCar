09ac9e2c53203b6064d20ff3097cb9a9699d21d0
sistema.noturno <sistema.noturno@LAB-03-19.sistemafieto.rede>
2025-09-02 21:02:41 -0300

    criação das views, criação dos pacotes; model, controller, dao, icons, image e criaçao dos construtores no pacote model.

--- Projeto_ServiCar/src/model/Prestador.java
+++ Projeto_ServiCar/src/model/Prestador.java
@@ -1,5 +1,113 @@
 package model;
 
 public class Prestador {
+	
+	private String id;
+	private String nome;
+	private String cpf_cnpj;
+	private String funcao;
+	private String telefone;
+	private String email;
+	private String comissao;
+	private String observacoes;
+	
+	
+	public Prestador(String id, String nome, String cpf_cnpj, String funcao, String telefone, String email,
+			String comissao, String observacoes) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cpf_cnpj = cpf_cnpj;
+		this.funcao = funcao;
+		this.telefone = telefone;
+		this.email = email;
+		this.comissao = comissao;
+		this.observacoes = observacoes;
+	}
+	
+	
+	
+	
+	public String getId() {
+		return id;
+	}
 
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getCpf_cnpj() {
+		return cpf_cnpj;
+	}
+
+
+	public void setCpf_cnpj(String cpf_cnpj) {
+		this.cpf_cnpj = cpf_cnpj;
+	}
+
+
+	public String getFuncao() {
+		return funcao;
+	}
+
+
+	public void setFuncao(String funcao) {
+		this.funcao = funcao;
+	}
+
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getComissao() {
+		return comissao;
+	}
+
+
+	public void setComissao(String comissao) {
+		this.comissao = comissao;
+	}
+
+
+	public String getObservacoes() {
+		return observacoes;
+	}
+
+
+	public void setObservacoes(String observacoes) {
+		this.observacoes = observacoes;
+	}
+
+
+	
+	
 }
--- Projeto_ServiCar/src/model/Veiculo.java
+++ Projeto_ServiCar/src/model/Veiculo.java
@@ -1,5 +1,100 @@
 package model;
 
 public class Veiculo {
+	
+	private String id;
+	private String placa;
+	private String marca;
+	private String modelo;
+	private String cor;
+	private String ano;
+	private String observacao;
+	
+	
+	public Veiculo(String id, String placa, String marca, String modelo, String cor, String ano, String observacao) {
+		super();
+		this.id = id;
+		this.placa = placa;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.cor = cor;
+		this.ano = ano;
+		this.observacao = observacao;
+	}
+
+
+	public String getId() {
+		return id;
+	}
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+	public String getPlaca() {
+		return placa;
+	}
+
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+
+	public String getMarca() {
+		return marca;
+	}
+
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+
+	public String getModelo() {
+		return modelo;
+	}
+
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+
+	public String getCor() {
+		return cor;
+	}
+
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+
+	public String getAno() {
+		return ano;
+	}
+
+
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+
+
+	public String getObservacao() {
+		return observacao;
+	}
+
+
+	public void setObservacao(String observacao) {
+		this.observacao = observacao;
+	}
+	
+	
+	
+	
+	
 
 }
