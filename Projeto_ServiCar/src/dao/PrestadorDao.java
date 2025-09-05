62a88e0fbd09ee8f98dab08c62a12708629a30c4
sistema.noturno <sistema.noturno@LAB-03-20.sistemafieto.rede>
2025-09-04 21:49:15 -0300

    Criação do Pacote DAO

---
+++ Projeto_ServiCar/src/dao/PrestadorDao.java
@@ -0,0 +1,150 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import ConnectionFactory.ConnectionDatabase;
+import model.Prestador;
+
+public class PrestadorDao {
+
+    public void create(Prestador prestador) {
+        Connection con = ConnectionDatabase.getConnection();
+        PreparedStatement stmt = null;
+//cv bfgbtbgtbaaa
+        try {
+            stmt = con.prepareStatement("INSERT INTO Prestador (nome, cpf_cnpj, funcao, telefone, email, comissao, observacoes) VALUES (?, ?, ?, ?, ?, ?, ?)");
+            stmt.setString(1, prestador.getNome());
+            stmt.setString(2, prestador.getCpf_cnpj());
+            stmt.setString(3, prestador.getFuncao());
+            stmt.setString(4, prestador.getTelefone());
+            stmt.setString(5, prestador.getEmail());
+            stmt.setString(6, prestador.getComissao());
+            stmt.setString(7, prestador.getObservacoes());
+
+            stmt.execute();
+            System.out.println("Prestador cadastrado!");
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao cadastrar prestador!", e);
+        } finally {
+            ConnectionDatabase.closeConnection(con, stmt);
+        }
+    }
+
+    public static ArrayList<Prestador> read() {
+        Connection con = ConnectionDatabase.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        ArrayList<Prestador> prestadores = new ArrayList<>();
+
+        try {
+            stmt = con.prepareStatement("SELECT * FROM Prestador");
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Prestador prestador = new Prestador(null, null, null, null, null, null, null, null);
+                prestador.setId(rs.getString("idPrestador"));
+                prestador.setNome(rs.getString("nome"));
+                prestador.setCpf_cnpj(rs.getString("cpf_cnpj"));
+                prestador.setFuncao(rs.getString("funcao"));
+                prestador.setTelefone(rs.getString("telefone"));
+                prestador.setEmail(rs.getString("email"));
+                prestador.setComissao(rs.getString("comissao"));
+                prestador.setObservacoes(rs.getString("observacoes"));
+
+                prestadores.add(prestador);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao ler os dados dos prestadores!", e);
+        } finally {
+            ConnectionFactory.ConnectionDatabase.closeConnection(con, stmt, rs);
+        }
+
+        return prestadores;
+    }
+
+    public void delete(int idPrestador) {
+        Connection con = ConnectionDatabase.getConnection();
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = con.prepareStatement("DELETE FROM Prestador WHERE idPrestador = ?");
+            stmt.setInt(1, idPrestador);
+
+            stmt.execute();
+            System.out.println("Prestador excluído!");
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao excluir prestador!", e);
+        } finally {
+            ConnectionDatabase.closeConnection(con, stmt);
+        }
+    }
+
+    public static ArrayList<Prestador> search(String pesquisar) {
+        Connection con = ConnectionDatabase.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        pesquisar = "%" + pesquisar + "%";
+        ArrayList<Prestador> prestadores = new ArrayList<>();
+
+        try {
+            stmt = con.prepareStatement("SELECT * FROM Prestador WHERE nome LIKE ? OR cpf_cnpj LIKE ?");
+            stmt.setString(1, pesquisar);
+            stmt.setString(2, pesquisar);
+
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Prestador prestador = new Prestador(pesquisar, pesquisar, pesquisar, pesquisar, pesquisar, pesquisar, pesquisar, pesquisar);
+                prestador.setId(rs.getString("idPrestador"));
+                prestador.setNome(rs.getString("nome"));
+                prestador.setCpf_cnpj(rs.getString("cpf_cnpj"));
+                prestador.setFuncao(rs.getString("funcao"));
+                prestador.setTelefone(rs.getString("telefone"));
+                prestador.setEmail(rs.getString("email"));
+                prestador.setComissao(rs.getString("comissao"));
+                prestador.setObservacoes(rs.getString("observacoes"));
+
+                prestadores.add(prestador);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao buscar prestadores!", e);
+        } finally {
+            ConnectionDatabase.closeConnection(con, stmt, rs);
+        }
+
+        return prestadores;
+    }
+
+    public void update(Prestador prestador) {
+        Connection con = ConnectionDatabase.getConnection();
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = con.prepareStatement(
+                "UPDATE Prestador SET nome = ?, cpf_cnpj = ?, funcao = ?, telefone = ?, email = ?, comissao = ?, observacoes = ? WHERE idPrestador = ?"
+            );
+
+            stmt.setString(1, prestador.getNome());
+            stmt.setString(2, prestador.getCpf_cnpj());
+            stmt.setString(3, prestador.getFuncao());
+            stmt.setString(4, prestador.getTelefone());
+            stmt.setString(5, prestador.getEmail());
+            stmt.setString(6, prestador.getComissao());
+            stmt.setString(7, prestador.getObservacoes());
+            stmt.setString(8, prestador.getId());
+
+            stmt.executeUpdate();
+            System.out.println("Prestador atualizado!");
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao atualizar prestador!", e);
+        } finally {
+            ConnectionDatabase.closeConnection(con, stmt);
+        }
+    }
+}
---
+++ Projeto_ServiCar/src/dao/ServicoDao.java
@@ -0,0 +1,189 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import ConnectionFactory.ConnectionDatabase;
+import model.Servico;
+
+public class ServicoDao {
+
+    public void create(Servico servico) {
+        Connection con = ConnectionDatabase.getConnection();
+        PreparedStatement stmt = null;
+//
+        try {
+            stmt = con.prepareStatement("INSERT INTO Servico (veiculo_id, prestador_id, data_servico, descricao, valor_total, valor_comissao, forma_pagamento, comprovante_path) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+
+            stmt.setString(1, servico.getVeiculo_id());
+            stmt.setString(2, servico.getPrestador_id());
+            stmt.setString(3, servico.getData_servico());
+            stmt.setString(4, servico.getDescricao());
+            stmt.setString(5, servico.getValor_total());
+            stmt.setString(6, servico.getValor_comissao());
+            stmt.setString(7, servico.getForma_pagamento());
+            stmt.setString(8, servico.getComprovante_path());
+
+            stmt.execute();
+            System.out.println("Serviço cadastrado com sucesso!");
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao cadastrar serviço!", e);
+        } finally {
+            ConnectionDatabase.closeConnection(con, stmt);
+        }
+    }
+
+    public static ArrayList<Servico> read() {
+        Connection con = ConnectionDatabase.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        ArrayList<Servico> servicos = new ArrayList<>();
+
+        try {
+            stmt = con.prepareStatement("SELECT * FROM Servico");
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Servico servico = new Servico(
+                    rs.getString("idServico"),
+                    rs.getString("veiculo_id"),
+                    rs.getString("prestador_id"),
+                    rs.getString("data_servico"),
+                    rs.getString("descricao"),
+                    rs.getString("valor_total"),
+                    rs.getString("valor_comissao"),
+                    rs.getString("forma_pagamento"),
+                    rs.getString("comprovante_path")
+                );
+                servicos.add(servico);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao ler os serviços!", e);
+        } finally {
+            ConnectionDatabase.closeConnection(con, stmt, rs);
+        }
+
+        return servicos;
+    }
+
+    public void delete(int id) {
+        Connection con = ConnectionDatabase.getConnection();
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = con.prepareStatement("DELETE FROM Servico WHERE idServico = ?");
+            stmt.setInt(1, id);
+
+            stmt.execute();
+            System.out.println("Serviço excluído com sucesso!");
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao excluir serviço!", e);
+        } finally {
+            ConnectionDatabase.closeConnection(con, stmt);
+        }
+    }
+
+    public void update(Servico servico) {
+        Connection con = ConnectionDatabase.getConnection();
+        PreparedStatement stmt = null;
+
+        try {
+            stmt = con.prepareStatement("UPDATE Servico SET veiculo_id = ?, prestador_id = ?, data_servico = ?, descricao = ?, valor_total = ?, valor_comissao = ?, forma_pagamento = ?, comprovante_path = ? WHERE idServico = ?");
+
+            stmt.setString(1, servico.getVeiculo_id());
+            stmt.setString(2, servico.getPrestador_id());
+            stmt.setString(3, servico.getData_servico());
+            stmt.setString(4, servico.getDescricao());
+            stmt.setString(5, servico.getValor_total());
+            stmt.setString(6, servico.getValor_comissao());
+            stmt.setString(7, servico.getForma_pagamento());
+            stmt.setString(8, servico.getComprovante_path());
+            stmt.setString(9, servico.getId());
+
+            stmt.executeUpdate();
+            System.out.println("Serviço atualizado com sucesso!");
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao atualizar serviço!", e);
+        } finally {
+            ConnectionDatabase.closeConnection(con, stmt);
+        }
+    }
+
+    public static ArrayList<Servico> search(String pesquisa) {
+        Connection con = ConnectionDatabase.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        ArrayList<Servico> servicos = new ArrayList<>();
+
+        pesquisa = "%" + pesquisa + "%";
+
+        try {
+            stmt = con.prepareStatement("SELECT * FROM Servico WHERE descricao LIKE ?");
+            stmt.setString(1, pesquisa);
+
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Servico servico = new Servico(
+                    rs.getString("idServico"),
+                    rs.getString("veiculo_id"),
+                    rs.getString("prestador_id"),
+                    rs.getString("data_servico"),
+                    rs.getString("descricao"),
+                    rs.getString("valor_total"),
+                    rs.getString("valor_comissao"),
+                    rs.getString("forma_pagamento"),
+                    rs.getString("comprovante_path")
+                );
+                servicos.add(servico);
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao buscar serviços!", e);
+        } finally {
+            ConnectionDatabase.closeConnection(con, stmt, rs);
+        }
+
+        return servicos;
+    }
+    public Servico findById(int id) {
+        Connection con = ConnectionDatabase.getConnection();
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+        try {
+            stmt = con.prepareStatement("SELECT * FROM Servico WHERE idServico = ?");
+            stmt.setInt(1, id);
+            rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                return new Servico(
+                    rs.getString("idServico"),
+                    rs.getString("veiculo_id"),
+                    rs.getString("prestador_id"),
+                    rs.getString("data_servico"),
+                    rs.getString("descricao"),
+                    rs.getString("valor_total"),
+                    rs.getString("valor_comissao"),
+                    rs.getString("forma_pagamento"),
+                    rs.getString("comprovante_path")
+                );
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao buscar serviço por ID!", e);
+        } finally {
+            ConnectionDatabase.closeConnection(con, stmt, rs);
+        }
+
+        return null;
+    }
+
+}
