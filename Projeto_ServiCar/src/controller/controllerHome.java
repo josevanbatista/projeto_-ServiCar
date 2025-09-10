565b429da9fbd2157c012a781e2f124eef3a1e7e
sistema.noturno <sistema.noturno@LAB-03-19.sistemafieto.rede>
2025-09-10 20:02:16 -0300

    atualização da classe " controllerhome"

--- Projeto_ServiCar/src/controller/controllerHome.java
+++ Projeto_ServiCar/src/controller/controllerHome.java
@@ -1,5 +1,45 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
 public class controllerHome {
+
+	@FXML
+	private Button btCadastroPrestadores;
+
+	@FXML
+	private Button btCadastroVeiculos;
+
+	@FXML
+	private Button btRegistroServicos;
+
+	@FXML
+	private Button btSair;
+
+	@FXML
+	private Text txtUser;
+
+	@FXML
+	void actionCadastroPrestadores(ActionEvent event) {
+
+	}
+
+	@FXML
+	void actionCadastroVeiculos(ActionEvent event) {
+
+	}
+
+	@FXML
+	void actionRegistroServicos(ActionEvent event) {
+
+	}
+
+	@FXML
+	void actionSair(ActionEvent event) {
+
+	}
 
 }
